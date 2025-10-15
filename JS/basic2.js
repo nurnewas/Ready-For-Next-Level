001904f9eee7f34b6317db1e7ef91423a373ef1c
Md Nur Newas <nurnewas@Mds-MacBook-Air.local>
2025-10-15 22:06:51 +0600

    templet string

---
+++ JS/basic2.js
@@ -0,0 +1,10 @@
+const students = {
+  name: "John",
+  age: 20,
+  address: "New York",
+  great: "A+",
+};
+
+console.log(
+  `the student name is ${students.name} .his age is ${students.age} and he lives in ${students.address}. .he got ${students.great} grade`
+);
